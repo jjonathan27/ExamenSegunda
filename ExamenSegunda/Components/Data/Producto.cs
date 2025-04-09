16ae9aaac83ab320a7a6ed11c84031fac23690d5
Jonathan Alejandro Flores Gómez <jflores84@ucol.mx>
2025-04-09 12:33:34 -0600

    Agregar archivos de proyecto.

---
+++ ExamenSegunda/Components/Data/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenSegunda.Components.Data
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El número de inventario es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe ser un número positivo")]
+        public int NumeroInventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(200, ErrorMessage = "El nombre no puede exceder 200 caracteres")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El departamento es obligatorio")]
+        [RegularExpression("Frutas|Verduras|Lácteos", ErrorMessage = "Departamento inválido")]
+        public string Departamento { get; set; } = string.Empty;
+    }
+}
